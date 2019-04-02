be0a80e147ea0ffee34890544822bc42e174bf98
feyd-rautha13 <42315752+feyd-rautha13@users.noreply.github.com>
2019-04-02 15:15:59 +0800

    test

--- Test/data process/test_WSS.py
+++ Test/data process/test_WSS.py
@@ -36,6 +36,46 @@ odd_trace_10dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Por
 even_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port1\\f_even_50G_15dB.txt"
 odd_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port1\\f_odd_50G_15dB.txt"
 
+#add port2 data path
+p2_even_trace_0dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_even_50G_0dB.txt"
+p2_odd_trace_0dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_odd_50G_0dB.txt"
+
+p2_even_trace_5dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_even_50G_5dB.txt"
+p2_odd_trace_5dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_odd_50G_5dB.txt"
+
+p2_even_trace_10dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_even_50G_10dB.txt"
+p2_odd_trace_10dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_odd_50G_10dB.txt"
+
+p2_even_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_even_50G_15dB.txt"
+p2_odd_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port2\\f_odd_50G_15dB.txt"
+
+#add port3 data path
+p3_even_trace_0dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_even_50G_0dB.txt"
+p3_odd_trace_0dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_odd_50G_0dB.txt"
+
+p3_even_trace_5dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_even_50G_5dB.txt"
+p3_odd_trace_5dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_odd_50G_5dB.txt"
+
+p3_even_trace_10dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_even_50G_10dB.txt"
+p3_odd_trace_10dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_odd_50G_10dB.txt"
+
+p3_even_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_even_50G_15dB.txt"
+p3_odd_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port3\\f_odd_50G_15dB.txt"
+
+#add port4 data path
+p4_even_trace_0dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_even_50G_0dB.txt"
+p4_odd_trace_0dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_odd_50G_0dB.txt"
+
+p4_even_trace_5dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_even_50G_5dB.txt"
+p4_odd_trace_5dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_odd_50G_5dB.txt"
+
+p4_even_trace_10dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_even_50G_10dB.txt"
+p4_odd_trace_10dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_odd_50G_10dB.txt"
+
+p4_even_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_even_50G_15dB.txt"
+p4_odd_trace_15dB = "D:\\project\\OFP2\\2018-04-27 WSS\\2019-03-29 data\\f_50G\\Port4\\f_odd_50G_15dB.txt"
+
+
 
 sys.path.append(component_path)
 from WSS import WSS
@@ -63,54 +103,112 @@ print("chn\t","freq\t","wavelength\t",
 
 start = time.time()
 
-for i in grid_50:
-    chx = WSS(i, channel_plan_path, even_trace_5dB, odd_trace_5dB, sp_pass, sp_block, grid_50G)
-    parameter = chx.channel_parameter()
+def testWss(channel_plan, even_trace, odd_trace, sp_pass, sp_block, grid):
+    for i in grid_50:
+        chx = WSS(i, channel_plan, even_trace, odd_trace, sp_pass, sp_block, grid)
+        parameter = chx.channel_parameter()
     
-    chn = parameter[0]
-    freq = parameter[1]
-    wavelength = parameter[2]
+        chn = parameter[0]
+        freq = parameter[1]
+        wavelength = parameter[2]
     
-    IL = chx.IL()
-    IL_cen = IL[0]
-    IL_best = IL[1]
-    IL_worst = IL[2]
-    IL_ripple = IL[3]
+        IL = chx.IL()
+        IL_cen = IL[0]
+        IL_best = IL[1]
+        IL_worst = IL[2]
+        IL_ripple = IL[3]
     
-    db_5 = chx.passband(0.5)
-    db_5p = db_5[6]
-    ccp_5 = db_5[7]
+        db_5 = chx.passband(0.5)
+        db_5p = db_5[6]
+        ccp_5 = db_5[7]
     
-    db_1 = chx.passband(1)
-    db_1p = db_1[6]
-    ccp_1 = db_1[7]
+        db_1 = chx.passband(1)
+        db_1p = db_1[6]
+        ccp_1 = db_1[7]
     
-    db_3 = chx.passband(3)
-    db_3p = db_3[6]
-    ccp_3 = db_3[7]
+        db_3 = chx.passband(3)
+        db_3p = db_3[6]
+        ccp_3 = db_3[7]
     
-    freq_shift = db_3[8]
+        freq_shift = db_3[8]
 
-    pdl = chx.pdl
-    pmd = chx.pmd
+        pdl = chx.pdl
+        pmd = chx.pmd
     
-    gdr = chx.gdr_cd(2)
-    cd = gdr[0]
-    gdr_1 = gdr[1]
+        gdr = chx.gdr_cd(2)
+        cd = gdr[0]
+        gdr_1 = gdr[1]
     
-    ER = chx.ER
+        ER = chx.ER
+
+        print(chn,freq,wavelength,
+              IL_cen,IL_best,IL_worst,IL_ripple,
+              db_5p,ccp_5,
+              db_1p,ccp_1,
+              db_3p,ccp_3,
+              freq_shift,
+              pdl,pmd,cd,gdr_1,
+              ER)
 
 
-    
-    print(chn,freq,wavelength,
-          IL_cen,IL_best,IL_worst,IL_ripple,
-          db_5p,ccp_5,
-          db_1p,ccp_1,
-          db_3p,ccp_3,
-          freq_shift,
-          pdl,pmd,cd,gdr_1,
-          ER)
-    
+
+#port3
+
+print("--------------------------------------------------------------------------------------------")
+print("------------------------------------Port3 test----------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+print("Port3 0dB test")
+testWss(channel_plan_path, p3_even_trace_0dB, p3_odd_trace_0dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+print("Port3 5dB test")
+testWss(channel_plan_path, p3_even_trace_5dB, p3_odd_trace_5dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+print("Port3 10dB test")
+testWss(channel_plan_path, p3_even_trace_10dB, p3_odd_trace_10dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+print("Port3 15dB test")
+testWss(channel_plan_path, p3_even_trace_15dB, p3_odd_trace_15dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+
+
+
+#port4 test
+
+print("--------------------------------------------------------------------------------------------")
+print("-----------------------------------Port4 test-----------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+
+print("Port4 0dB test")
+testWss(channel_plan_path, p4_even_trace_0dB, p4_odd_trace_0dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+print("Port4 5dB test")
+testWss(channel_plan_path, p4_even_trace_5dB, p4_odd_trace_5dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+print("Port4 10dB test")
+testWss(channel_plan_path, p4_even_trace_10dB, p4_odd_trace_10dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+print("Port4 15dB test")
+testWss(channel_plan_path, p4_even_trace_15dB, p4_odd_trace_15dB, sp_pass, sp_block, grid_50G)
+print("--------------------------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------")
+
+
 stop = time.time()
 print (stop - start)  
 
