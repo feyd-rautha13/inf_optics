dd9ed6906b08544e84a8eb3814686e8368c2a0de
feyd-rautha13 <42315752+feyd-rautha13@users.noreply.github.com>
2019-05-31 12:43:48 +0800

    Add FRB-8 driver draft script.

--- Test/component/transceiver.py
+++ Test/component/transceiver.py
@@ -16,8 +16,12 @@ __version__ = "1.0"
 
 import time
 import re
+import openpyxl
 
 class QSFPDD():
+    '''
+    Compatible with  QSFP-DD Common Management specification Rev 3.0
+    '''
     def __init__(self, interface, card = 1, port = 8):
         '''
         initial QSFP-DD;
@@ -27,41 +31,206 @@ class QSFPDD():
         self.port = port
         self.dev = interface
 
-    def reg_get(self, page = 0x00, offset = 0x00):
-        '''
-        Get specified register content, return a number in DEC base
-        '''
-        address = format(page**256+offset, '#06x')
+    def reg_set(self, page = 0x00, offset = 0x00, value = 0x00):
         
-        cmd = "hw {0} qsfp {1} rr {2} 1".format(self.card, self.port, address)
+        address = format(page*256+offset, '#06x')
+        cmd = "hw {0} qsfp {1} ww {2} {3}".format(self.card, self.port, address, value)
         self.dev.write(cmd)
-        time.sleep(1)
+    
+    def reg_get(self, page = 0x00, offset = 0x00, length = 128):
         
+        address = format(page*256+offset, '#06x')
+        
+        cmd = "hw {0} qsfp {1} rr {2} {3}".format(self.card, self.port, address, length)
+        
+        self.dev.clear_buffer()
+        self.dev.write(cmd)
         data = self.dev.read_raw().decode()
         data = re.split(r'\r|\n', data)
-        data = [x for x in data if x != '']
-        data = data[1].replace("[{0}]: ".format(address), "")
-        data = int('0x'+data, 16)
+        data = [x for x in data if x != ''][1: length+1]
+        data = [ x[-2:] for x in data]
         
         return data
+    
+    def password_entry(self):
+        self.reg_set(0,118,0x00)
+        self.reg_set(0,119,0x00)
+        self.reg_set(0,120,0x10)
+        self.reg_set(0,111,0x11)
+        self.dev.clear_buffer()
 
-    def reg_get2(self, page = 0x00, offset = 0x00):
+#Table-18 Table-19   
+    @property
+    def ModState(self):
+        self.dev.clear_buffer()
+        data = int(self.reg_get(0,3,1)[0],16)
+        data = (data>>1 & 0x07)
+        
+        if data==1:
+            return 'Module LowPwr state'
+        elif data == 2:
+            return 'Module PwrUp state'
+        elif data == 3:
+            return 'Module Ready state'
+        elif data == 4:
+            return 'Module PwrDn state'
+        elif data == 5:
+            return 'Module Fault state'
+        else:
+            return 'Reserved'
+        
+#Table 22 Module level Monitor
+    @property
+    def ModMonCaseTemp(self):
         '''
-        Get specified register content, return a number in DEC base
+        later. 
         '''
-        address = format(page**256+offset, '#06x')
+        self.dev.clear_buffer()
+        data = self.reg_get(0,14,2)      
+        data = (int(data[0],16)*256+int(data[1],16))/256
+        if data<128:
+            return data 
+        else: 
+            return data-128
+    
+    @property
+    def ModMonInpVcc(self):
+        self.dev.clear_buffer()
+        data = self.reg_get(0,16,2)
+        data = (int(data[0],16)*256+ int(data[1],16))*100/1E6
         
-        cmd = "hw {0} qsfp {1} rr {2} 1".format(self.card, self.port, address)
-        self.dev.write(cmd)
-        time.sleep(1)
+        return data        
         
-        data = self.dev.read_raw().decode()[-9:-7]
-
-        data = int('0x'+data, 16)
+    @property
+    def ModMonLasTemp(self):
+        self.dev.clear_buffer()
+        data = self.reg_get(0,20,2)
+        data = (int(data[0],16)*256+ int(data[0],16))/256
+        if data<128:
+            return data    
+        else: 
+            return data-128    
+#Table 27
+    @property
+    def ModVendorName(self):
+        self.dev.clear_buffer()
+        s = ''
+        data = self.reg_get(0,129,16)
+        data = [chr(int(x,16)) for x in data]
+        for i in data:
+            s +=i
+        return s
+    
+    @property
+    def ModVendorPN(self):
+        self.dev.clear_buffer()
+        s = ''
+        data = self.reg_get(0,148,16)
+        data = [chr(int(x,16)) for x in data]
+        for i in data:
+            s +=i
+        return s        
+    
+    @property    
+    def ModVendorSN(self):
+        self.dev.clear_buffer()
+        s = ''
+        data = self.reg_get(0,166,16)
+        data = [chr(int(x,16)) for x in data]
+        for i in data:
+            s +=i
+        return s       
+    
+    @property    
+    def ModVendorSN2(self):
+        self.dev.clear_buffer()
+        s = ''
+        data = self.reg_get(0,196,16)
+        data = [chr(int(x,16)) for x in data]
+        for i in data:
+            s +=i
+        return s    
         
-        return data
+    @property    
+    def ModDateCode(self):
+        self.dev.clear_buffer()
+        s = ''
+        data = self.reg_get(0,182,8)
+        data = [chr(int(x,16)) for x in data]
+        for i in data:
+            s +=i
+        return s        
+        
+#Table 31        
+    @property
+    def ModCableLength(self):
+        self.dev.clear_buffer()
+        data = int(self.reg_get(0,202,1)[0],16)
+        multiplier = (data & 0xC0)>>5
+        baselength = data & 0x1F
+        
+        if multiplier==0:
+            return baselength*0.1
+        elif multiplier == 1:
+            return baselength*1
+        elif multiplier == 2:
+            return baselength*10
+        elif multiplier == 3:
+            return baselength*100
+        else:
+            return 'Reserved'        
+        
+#Table 32         
+    @property
+    def ModMediaType(self):
+        self.dev.clear_buffer()
 
+        data = self.reg_get(0,203,1)[0]
 
+        return data   
 
+#Module Infomation
+    def ModInfo(self):
+        self.dev.clear_buffer()
+        print("Vendor Name\t",self.ModVendorName)
+        print("Vendor PN\t", self.ModVendorPN)
+        print("Vendor SN\t",self.ModVendorSN)
+        print("Vendor Date\t",self.ModDateCode)
 
-
+#CRC code
+    def GetEEpromString(self, path, sh_order):
+        wb = openpyxl.load_workbook(path)
+        sheet = wb[wb.sheetnames[sh_order]]
+        s=[]
+        string = ''
+        for i in range(2,120):
+            addr = 'G'+str(i)
+            s.append(str(sheet[addr].value))
+            for i in s:
+                string += i
+                wb.close()
+        return string
+    
+    def GenCRCCode(self, data):
+        length = int(len(data)/2)
+        crc = 0xffff
+        for i in range(length):
+            flag = int(data[2*i:2*i+2],16)
+            crc ^= flag
+            for i in range(8):
+                last = crc%2
+                crc >>= 1
+                if last==1:
+                    crc ^= 0xa001
+        return hex(crc)
+    
+    def CRCcheck(self, data):
+        eeprom = self.reg_get(0,128,117)
+        s=''
+        for i in eeprom:
+            s += i
+        return s
+    
+    
+    
+    
--- labdevice/FTB-8.py
+++ labdevice/FTB-8.py
@@ -5,7 +5,7 @@ __author__ = 'Sizhan Liu'
 __version__ = "1.0"
 
 '''
-Luna OVA5000 interface
+EXFO FTB-8 Chasis interface
 '''
 
 path1 =  'D:\\work\\coding\\python\\inf_optics\\interface\\'
@@ -18,19 +18,8 @@ import re
 import pylab as pl
 from TCPinterface import TCP
 
-# debug only
-# luna = Luna(ip['host'], ip['port'])
-#ip = {
-#    'host' : "10.13.51.252", 
-#    'port' : 1
-#    }
 
-#testplan = {
-#    'center': {'C':1549.0,'C+L':1569.0},
-#    'range':{'C': 41.67,'C+L':85.69}
-#    }
-
-ip = '10.13.11.33'
+ip = '172.29.150.191'
 port = 5025
 
 ############### --Connectiong -- ###############
@@ -55,7 +44,7 @@ class FTB8(TCP):
         #    self.close()
 
     def close(self):
-        '''Close remote connection for OVA5000'''
+        '''Close remote connection for FTB-8'''
         self.write("*QUIT")
         self.TCP_close()
         print(self.__class__.__name__ + ' had been disconnected!')
@@ -65,28 +54,29 @@ class FTB8(TCP):
         return command.decode().replace("\x00",'')
 
 ########## --super class alternative method --- #######
-    def read(self):
-        '''
-        The Luna response time may larger than TCP time out time.
-        Luna will response a binary list with EOS "\x00". 
-        '''
-        data = TCP.read_raw(self)
-        while True:
-            try:
-                if len(data) <= 1 or data[-1]!=0:
-                    data += TCP.read_raw(self)
-                else:
-                    break
-                #return data    
-            except:
-                pass
-        return data       
+#    def read(self):
+#        '''
+#        The Luna response time may larger than TCP time out time.
+#        Luna will response a binary list with EOS "\x00". 
+#        '''
+#        data = TCP.read_raw(self)
+#        while True:
+#            try:
+#                if len(data) <= 1 or data[-1]!=0:
+#                    data += TCP.read_raw(self)
+#                else:
+#                    break
+#                #return data    
+#            except:
+#                pass
+#        return data       
     
     def query(self,cmd):
         '''query result from Luna'''
+        cmd = str(cmd)+"\n"
         self.write(cmd)
-        #time.sleep(1)
-        data = self.read()
+#        time.sleep(0.5)
+        data = self.read_raw()
         return data
 
     def inspection(self,command ="SYST:ERR?" ,exception = '0'):
@@ -103,223 +93,36 @@ class FTB8(TCP):
                 pass
 
 
-#################---Basic Field ---###############
+#################---Traffic analyser ---###############
     @property
     def deviceID(self):
         return self.query("*IDN?")
+    def CLS(self):
+        self.write("*CLS\n")
     
-    @property
-    def centerWav(self):
-        '''center wavelength config'''
-        command = "CONF:CWL?"
-        return self.query(command)
-    @centerWav.setter
-    def centerWav(self,center):
-        command="CONF:CWL"+ " "+ "%s" % center
-        self.write(command)
-
-    @property
-    def startWav(self):
-        '''get start wavelength'''
-        command = "CONF:STAR?"
-        return self.query(command)
-    @startWav.setter
-    def startWav(self,start):
-        '''get start wavelength'''
-        command = "CONF:STAR"+" "+ "%s" % start
-        self.write(command)
-
-    @property
-    def rangeWav(self):
-        '''get wavelength range'''
-        command = "CONF:RANG?"
-        return self.query(command)
-    @rangeWav.setter
-    def rangeWav(self,rangewav):
-        '''wavelength range config '''
-        command = "CONF:RANG"+" "+"%s" % rangewav
-        self.write(command)
-
-    @property
-    def stopWav(self):
-        '''get stop wavelength'''
-        command = 'CONF:END?'
-        return self.query(command)
+    def BERperLane(self, laneID=0):
+        self.CLS()
+        cmd = "LINS5:FETC:DATA:TEL:PATT:ERR:RATE? {0},BIT".format(laneID)
+        data = self.query(cmd)
+        data = float(data.decode().replace('\n',''))
+        return data
     
-    @property
-    def measInfor(self):
-        '''get OVA current meassurement detials for Matrix A P166'''
-        command = "FETC:MDET?"
-        return self.query(command)
-
-################ -- pre-Measurement Setting-- ###############
-    def measType(self):
-        '''set measure type as transmission'''
-        command = "CONF:TEST 1,1"
-        self.write(command)
-        self.inspection(command = "CONF:TEST? 0", exception = "1")
-        print("--> Measurement type is set to transmission with full calibration.")
-
-    def measAverage(self, number = 10):
-        '''Enable Matrix average and set average times '''
-        #eanble matrix average
-        command = "CONF:AVGE 1"
-        self.write(command)
-        self.inspection(command = "CONF:AVGE? 0", exception = "1")
-        print("--> Matrix Average is enabled.")
-
-        #set average number = 10
-        command = "CONF:AVGS" +" " + "%s" % number
-        self.write(command)
-        self.inspection(command = "CONF:AVGS? 0", exception = str(number))
-        print("--> Matrix Average 10 times.")
+    def CurrentBERState(self, lane=8):
+        for i in range(8):
+            print("Lane {0} BER = ".format(i), self.BERperLane(i))
         
-
-    def measFindDutLength(self):
-        '''Enable auto find dutlength, set find dut length method to broadband'''
-        #Eable auto find DUT length
-        command = "CONF:FDUT 1"
-        self.inspection(command = "CONF:FDUT?", exception = "1")
-        print("--> Auto find DUT lenght is enabled.")
-
-        #Set find DUT length using broadband method
-        self.write(command)
-        command = "CONF:DUTM 0"
-        self.write(command)
-        self.inspection(command = "CONF:DUTM?", exception = "0")
-        print ("--> Using broadband method to find DUT length.")
-
-    def measApplyFilter(self):
-        '''Always apply filter on'''
-        command = "CONF:AAF 1"
-        self.write(command)
-        self.inspection(command = "CONF:AAF?", exception = "1")
-        print("--> Always apply filter ON.")
-
         
-    def measTimeDomainWindow(self):
-        '''Apply time domann window'''
-        command = "CONF:TDW 1"
-        self.write(command)
-
-
-################# --Measurement method-- ####################
-
-    def measDetail(self):
-        '''Get measurement deail from OVA5000'''
-        command = "FETC:MDET?"
-        lbuffer = self.query(command)
-        print (lbuffer.decode())
-
-    def setDUTName(self, dutname = ""):
-        '''set DUT name'''
-        command = "CONF:DUTN"+" "+"\""+dutname+"\""
-        self.write(command)
-        self.inspection(command = "CONF:DUTN?", exception = dutname)
-        print('--> DUT name is %s.' % dutname)
-
-    def scan(self):
-        '''Tells the OVA to execute an optical scan based on the
-            configured system parameters. Depending on the
-            wavelength range setting and the number of averages, this
-            command may take up to several minutes to complete.
-        '''
-        command = "SYST:RDY?"
-        self.inspection(command, exception = "1")
-        print ("--> It's ready to start scan..")
-        #input("--> Press Enter to start scan...")
- 
-        command = "SCAN"
-        scanStartTime = time.time()
-        self.write(command)
         
-        self.inspection()
-        scanStopTime = time.time()
-        print ("--> Scan is completed!  It takes %s secods." % (scanStopTime-scanStartTime))
-
-
-################# --Filter setting -- ###################
-
-#smoothing filter setting
-    def setSmoothFilter(self):
-        '''set smoothing filter bandwidth uint to gigahertz P179'''
-        command = "CONF:RSBU 1"
-        self.write(command)
-        self.inspection(command = "CONF:RSBU?", exception= '1')
-        print ("--> Smoothing filter bandwidth unit is GHz.")
-
         
-    
-    def setSmoothFiltertoMatirxA(self):
-        #set smoothing filter bandwidth value to 5pm
-        command = "CONF:FRBW 0.62"
-        self.write(command)
-        self.inspection(command = "CONF:FRBW?", exception = '0.620000')
-        print("--> Smoothing filter resolution bandwidth is setted to 5pm/0.62GHz")
-
-        '''Apply smoothing filter to Matrix A P168'''
-        command = "SYST:FILT"
-        self.write(command)
         
-       
-
-#Time domain filter
-    def setTimeDomain(self):
-        '''Confige time domain window, Hanning window must be set first because the setting would affect time
-            domain setting, i don't know why. Just do it.
-        '''
         
-        #diable Hanning window
-        '''diable hanning window for narrow band component, 1: enable, 0: disable'''
-        ''''''
-        command = "CONF:THAN 1"
-        self.write(command)
-        self.inspection(command = "CONF:THAN?", exception = '1')
-        print("--> Hanning window is enabled.")
-
-        #Enable time domain filter
-        command  = "CONF:TDW 1"
-        self.write(command)
-        self.inspection(command = "CONF:TDW?", exception = '1')
-        print("--> Time domain filter is Enabled.")
-
-        #Retain Settings in Time Domain Window
-        command = "CONF:TRET 1"
-        self.write(command)
-        self.inspection(command = "CONF:TRET?", exception = "1")
-        print("--> Time domain setting is maintained. ")
-
-
-################# --Save result -- ####################
-
-
-    def savetxtResult(self, filename='D:\\ch1'):
-        '''save txt file to local computer path'''
-        command = 'SYST:SAVT'
-        self.write(command+" "+filename+'.txt')
-        time.sleep(8)
-    def savebinResult(self, filename='D:\\ch1'):
-        command = 'SYST:SAVJ'
-        self.write(command+" "+filename+'.bin')
-        time.sleep(8)
-
-################# --data fetch -- ####################    
-    def fetchresult(self, items = 0):
-        '''0: IL; 1: GD; 3: PDL; 4:PMD; 11: Min/Max Loss; P153'''
-        command = 'FETC:MEAS? '+str(items)
-        IL = self.query(command)
-        IL = self.data_pasre(IL)
-        IL = re.split(r"\r|\t",IL)
-        IL = [float(i) for i in IL if i!='']
         
-        if items == 0:
-            pl.ylim(-20,0), pl.grid(),pl.plot(IL), pl.show()
-        else:
-            pass
-    
-        strtmp = input("input 'n' to disconnet to re-calibrate or \npress any key to continue..")
-        if strtmp.lower()=='n':
-            self.close()
-        else:
-            pass
-        return IL
+        
+        
+        
+        
+        
+        
+        
+        
+        
