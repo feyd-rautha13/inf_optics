3e75002c2630774f4e1737d4862d20bb71da1413
feyd-rautha13 <42315752+feyd-rautha13@users.noreply.github.com>
2019-06-14 17:17:09 +0800

    add alternative "write" for luna

--- Test/measure/Luna_measure_script.py
+++ Test/measure/Luna_measure_script.py
@@ -13,92 +13,57 @@ import time
 
 #device IP
 ip = {
-    'host' : "10.13.51.252", 
+    'host' : "10.13.11.254", 
     'port' : 1
     }
 
-#C band and C+L band test plan
-testplan = {
-    'center': {'C':1549.0,'C+L':1569.0},
-    'range':{'C': 41.67,'C+L':85.69}
-    }
 
+#Remote folder path
+path = "C:\\Users\\lab\\Desktop\\New folder\\"
+# device Descriptor
+dutname = 'tt'
 
-def test():
+def test(filepath, dutname):
     #Luna initial
+    astart = time.time()
     luna = Luna(ip['host'], ip['port'])
 
     #Test init
     luna.measType()   
     luna.measAverage(number = 10) 
-
     luna.measFindDutLength()
     
-    #Test parameter 
-    #should set start and range, center range would not affect test plan
-    #startwav = 1527.38
-    #luna.startWav = startwav
-
-    #Luna.centerWav = testplan['center']['C']
-    #luna.rangeWav = testplan['range']['C']
-
-    #Apply filter
-    luna.setSmoothFilter()
+    #Apply Time domain filter, Remeber the current time domain filter
     luna.setTimeDomain()
 
     #set DUT name
-    dutname = input("--> please enter dut name:")
     luna.setDUTName(dutname)
     #Start test
     luna.scan()
-
-    #set smoothing filter to Matrix A
-    luna.measApplyFilter()
-    luna.setSmoothFiltertoMatirxA()
     
-    time.sleep(5)
+    time.sleep(2)
+    
+    #check if scan finished
     print(luna.query("*OPC?"))
     
-    '''check if something wrong.'''   
-    luna.fetchresult()
+    '''check if something wrong.''' 
     #print measurement information
-    #print (luna.measInfor.decode().replace("\x00",""))
+    luna.measDetail()
 
-    #print measurement information
-    #print('Device ID: ', luna.data_pasre(luna.deviceID))
-    #print('Center Wavelength: ', luna.data_pasre(luna.centerWav))
-    print('--> Start wavelength: ', luna.data_pasre(luna.startWav))
-    #print('Scan Range: ', luna.data_pasre(luna.rangeWav))
-    print('--> Stop wavelength: ', luna.data_pasre(luna.stopWav))
-    #print('Firmware version: ', luna.data_pasre(luna.query("SYST:VER?")))
-    #print('Start frequency and increasment: ', luna.data_pasre(luna.query(''"FETC:FREQ?")))
-    #print('Time increasement: ', luna.data_pasre(luna.query("FETC:TINC?")))
-    #print('Time domain window resolution bandwidth: ', luna.data_pasre(luna.query("CONF:TWRB?")))
-    #print('Convolved Res BW: ', luna.data_pasre(luna.query("CONF:CRBW?")))
-    #print('Sample resolution: ', luna.data_pasre(luna.query("CONF:SRES?")))
-    #print('Time domain sigma', luna.data_pasre(luna.query("CONF:TSIG?")))
-    #print('Time domain start / stop: ', luna.data_pasre(luna.query("CONF:TLIM?"))) #time domain start / stop
-    #print('smoothing filter bandwidth unit', luna.data_pasre(luna.query("CONF:RSBU?"))) #smoothing filter bandwidth unit
-    print('--> smoothing filter value', luna.data_pasre(luna.query("CONF:FRBW?"))) #smoothing filter value
-
+    #luna.fetchresult()
 
     #File save
     #input("--> Press anykey to store data files..")
-    filepath = "C:\\Data\\sliu3\\Documents\\work\\test\\P0\\mux\\"+dutname
-    #luna.savebinResult(filename=filepath)
+    filepath = filepath+dutname
+    luna.savebinResult(filename=filepath)
     #print(luna.query("*OPC?"))
     luna.savetxtResult(filename=filepath)
     print(luna.query("*OPC?"))
     
     #close connection
-    #luna.close()
-
-
-if __name__ == "__main__":
-    astart = time.time()
-    test()
+    luna.close()
     astop = time.time()
-    
     print ("test time = ", astop - astart)
+
 
 
