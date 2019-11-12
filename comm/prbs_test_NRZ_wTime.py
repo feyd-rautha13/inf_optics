1b617ea23802f7ed5944d0739e8b2357cf94ab51
Sizhan Liu <SiLiu@infinera.com>
2019-11-12 18:28:17 +0800

    update

--- comm/prbs_test_NRZ_wTime.py
+++ comm/prbs_test_NRZ_wTime.py
@@ -8,14 +8,31 @@ Created on Sat Nov  9 22:23:09 2019
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
-#from commpy.filters import rrcosfilter
+#from commpy.filters import rcosfilter
 
-pattern_length = 200
+
+#############  Signal feature  #################
+baudrate = 10000 # symbol per second symbol/s
+Tsym = 1/baudrate # symbol period 
+
+trans_time = 1
+
+############# sample feature ##################
+sample_num = 2**5+1
+
+ts = Tsym/(sample_num -1) #1/fs # sample interval in one period 
+fs = 1/Tsym*(sample_num -1) #1/Tsym*100 # sample frequency in one period
+
+######### patter config ############
 prbs_type_1 = 31
 prbs_type_2 = 13
-Ts = 101
+pattern_length = int(baudrate*trans_time) #symbol time*baudrate
+
+########### pluse shaping filter ############
 edge = 0.3
-fsz = (8,5)
+
+########## figure config ###########
+fsz = (8,5) #figure size
 
 #PRBS pattern sequence generation
 def PRBS_gen(prbstype, pattern_length):
@@ -36,8 +53,8 @@ def encoder_grey(data):
     return grey_output
 
 #pluse generation
-def gen_pulse(seq, Ts):
-    a1 = int((Ts-1)/2)
+def gen_pulse(seq, sample_num):
+    a1 = int((sample_num-1)/2)
     allzero = np.zeros((a1,len(seq)))
     matrix1 = np.vstack((allzero, seq, allzero))
     matrix2 = np.reshape(matrix1, -1, order='F')
@@ -57,29 +74,46 @@ def pulse_shape(high, low, pre_z, pre_raise, post_fall, post_z, total):
 
 
 #show eye
-def plot_eye(data, Ts, offset):
+def plot_eye(data, sample_num, offset):
     plt.figure(figsize=fsz)
-    x_range = int((len(data) - Ts + 1)/Ts)
+    x_range = int((len(data) - sample_num + 1)/sample_num)
    
     for i in np.arange(x_range-1):
         try:
-            plt.plot(data[(offset+2*Ts*i):(offset+2*Ts*(i+1))])
+            plt.plot(data[(offset+2*sample_num*i):(offset+2*sample_num*(i+1))])
         except:
             pass
     plt.title('eye')
     plt.show()
 
-def plot_spectrum(data, Ts, title):
+def plot_spectrum(data, sample_num, title):
     plt.figure(figsize=fsz)
     zz = 20*np.log10(np.fft.fft(data))
-    plt.plot(zz[:Ts])
+    plt.plot(zz[:sample_num])
     plt.title('{0} Specturm'.format(title))
     plt.show()
-    
-    
-def Noise_gen(length, sigma):
-    return np.random.normal(0, sigma, (1, length))[0]
-    
+
+
+
+seq1 = PRBS_gen(prbs_type_1, pattern_length)
+seq2 = gen_pulse(seq1, sample_num)
+t_axis = ts*np.arange(len(seq2))
+
+
+
+p_shape = pulse_shape(1,0, 0,int(sample_num*edge),int(sample_num*edge), 0, int(sample_num*(1+edge)))
+
+
+
+final_1 = np.convolve(seq2, p_shape)
+t_axis_final = ts*np.arange(len(final_1))
+
+fftfft = 20*np.log10(np.fft.fftshift(np.fft.fft(final_1)))
+fff =fs* (np.arange(len(final_1)) - len(final_1)/2 + 1)/ len(final_1) 
+
+#plt.plot(p_shape)
+#plot_eye(final_1, sample_num, 0)
+#plot_spectrum(final_1, sample_num, 'NRZ')
 
 
 
