9ce993004023156542f97631cb938ce9d13d7453
Sizhan Liu <SiLiu@infinera.com>
2019-11-20 22:42:32 +0800

    update

--- comm/Tx_pam4_3.py
+++ comm/Tx_pam4_3.py
@@ -11,7 +11,7 @@ from scipy import signal
 import numpy as np
 
 n=1
-baudrate = 1000
+baudrate = 1
 run_point = 10000*n
 samples = 2**5+1
 
@@ -22,49 +22,64 @@ samples = 2**5+1
 #-3 instance
 tr_x = Transmitter(baudrate,run_point,samples)
 #sequence
-s0 = tr_x.genPRBS(31, run_point)
+s0 = tr_x.genPRBS(7, 20)
+'''
+plt.stem(s0)
+'''
 
 #-2 encoder
-grey_seq = tr_x.genGreySerial(s0,2**n)
+#grey_seq = tr_x.genGreySerial(s0,2**n)
+grey_seq = np.array([1,0,1,0,1,0,1,0,1,0])
 '''
-plt.plot(grey_seq[:100])
-plt.plot(grey_seq[900:])
+plt.stem(grey_seq)
 '''
 
 #grey_seq = tr_x.genGrey(s0,s1)
 
 #-1 pulse shapiping time domain
-xt_pshape, amp_pshape = tr_x.pulseShape(1,0, 0.25,0.25,33, tr_x.ts)
+xt_pshape, amp_pshape = tr_x.pulseShape(1,0, 0.1,0.1,samples, tr_x.ts)
 '''
 plt.plot(amp_pshape)
 '''
 #0 impulse generation time domain
 xt_grey_impulse, amp_grey_impulse = tr_x.genImpuls(grey_seq, tr_x.sample_num, tr_x.ts)
+rdt = np.diff(amp_grey_impulse)
+rdit = np.cumsum(rdt)
+'''
+plt.plot(amp_grey_impulse)
+plt.plot(rdt)
+plt.plot(rdit)
+'''
 
 #1 waveform generate time domain
 xt_pam, wav_pam = tr_x.genWavform(amp_grey_impulse, amp_pshape, tr_x.ts)
 
 
-'''
-plt.plot(wav_pam[:99])
-plt.plot(wav_pam[32000:])
-'''
-for i in np.arange(1,30,1):
 
-    #2 waveform with noise generate
-    xt_pam_noz, wav_pam_noz = tr_x.genWaveWithNoise(wav_pam, i, tr_x.ts)
-    
-    #3 match filter
-    xt_match, amp_match_filter = tr_x.matchFilter(amp_pshape, tr_x.ts)
-    
-    #4 seq pass match filter from step 2
-    xt_r_wav, amp_r_wav = tr_x.genRxWaveform(wav_pam_noz, amp_match_filter, tr_x.ts)
-    
-    sample_r = tr_x.sampleRx(amp_r_wav, samples, 66)
-    decision_out = tr_x.decisionNRzRx(sample_r, 0.50)
-   
-    ber = tr_x.BERcount(grey_seq, decision_out)
-    print (ber)
+'''
+plt.figure(figsize=(8,5))
+amp1 = np.zeros(19)
+amp2 = np.concatenate((amp1,amp_grey_impulse))
+plt.stem(amp2, use_line_collection=True)
+plt.plot(wav_pam, 'r')
+
+'''
+#for i in np.arange(1,30,1):
+#
+#    #2 waveform with noise generate
+#    xt_pam_noz, wav_pam_noz = tr_x.genWaveWithNoise(wav_pam, i, tr_x.ts)
+#    
+#    #3 match filter
+#    xt_match, amp_match_filter = tr_x.matchFilter(amp_pshape, tr_x.ts)
+#    
+#    #4 seq pass match filter from step 2
+#    xt_r_wav, amp_r_wav = tr_x.genRxWaveform(wav_pam_noz, amp_match_filter, tr_x.ts)
+#    
+#    sample_r = tr_x.sampleRx(amp_r_wav, samples, 66)
+#    decision_out = tr_x.decisionNRzRx(sample_r, 0.50)
+#   
+#    ber = tr_x.BERcount(grey_seq, decision_out)
+#    print (ber)
 
 
 
